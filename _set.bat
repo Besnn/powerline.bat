6f1f6a2962e73ff054b9beecf5760b0c13bb052a
besnn <besniknuro74@gmail.com>
2021-04-03 22:06:35 +0200

    Prompt color fix

--- _set.bat
+++ _set.bat
@@ -1,10 +1,11 @@
         @echo off
-        for %%a in ("%CD%") do set "PARENT_FOLDER=%%~nxa"
+        for %%a in ("%CD%") do set "PARENT_FOLDER=%%~nxa" :: selects parent folder by iterating through list
+	:: %%~nxa => echo filename + ext //weird way
         title %PARENT_FOLDER%
         set GITBRANCH=
         for /f "tokens=2" %%I in ('git.exe branch 2^> NUL ^| findstr /b "* "') do set GITBRANCH=%%I
         if "%GITBRANCH%" == "" (
-            prompt $E[30;44m$S$E[0m$E[30;44m$P$S$E[0m$E[34m$E[0m$S
+            prompt $E[30;104m$S$E[0m$E[30;104m$P$S$E[0m$E[1;94m$E[0m$S
         ) else (
-            prompt $E[30;44m$S$E[0m$E[30;44m$P$S$E[0m$E[34;43m$S$E[0m$E[30;43m%GITBRANCH%$S$E[0m$E[33m$E[0m$S
+            prompt $E[30;104m$S$E[0m$E[30;104m$P$S$E[0m$E[1;94;43m$S$E[0m$E[30;43m%GITBRANCH%$S$E[0m$E[33m$E[0m$S
         )
